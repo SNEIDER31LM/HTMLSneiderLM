874f9869bcb44ce1f4b01621dedc0af3b26730a8
SNEIDER31LM <holaquetallooctrex31@gmail.com>
2024-06-18 11:00:36 -0500

    html

---
+++ index.js
@@ -0,0 +1,10 @@
+fuction onClickSubmitButton(){
+    let inputValue = document.getElementById('inputText').value;
+    if(inputValue<0){
+        alert('Hey el numero es mayor a cero');
+
+    if(inputValue>0) {
+        alert('Hey el numero es menor a cero');
+
+    if(inputValue==0) {
+        alert('Hey el numero es mayor a cero');
